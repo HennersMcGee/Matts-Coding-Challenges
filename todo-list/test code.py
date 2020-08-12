b85aa9e53cdecee17d830e8f415057c3db78969b
HennersMcGee <henry_letton@hotmail.com>
2020-08-12 08:43:50 +0100

    Test code for todo-list

---
+++ todo-list/test code.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Title: Test Code
+
+Created on Wed Aug 12 08:02:34 2020
+
+@author: Henry
+"""
+
+## Import any packages
+
+## Define classes
+# To do list
+    # Inherit features from normal list but add functions (maybe sort)
+    # View overdue and due soon
+    # View full list
+# To do item
+    # Add full details of item
+
+## Read in list from last run (or create a new empty list)
+# Maybe ask user if they want to use previous list
+# Print if using previous
+
+## While loop for user to make as many additions or changes as they want
+
+    # Give user a numeric list of possible options
+    
+    # For chosen option, ask for any additional details
+    
+    # Complete chosen option
+    
+    # Exit loop if they pick the option about no more changes
+
+## Save list locally
